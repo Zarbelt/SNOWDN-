8f5d4ba0d0507a329d0e266aa71a102f70445917
Akihiko <akihikowiththenfts@gmail.com>
2025-02-07 21:11:35 +0100

    update

---
+++ script.js
@@ -0,0 +1,237 @@
+// Exchange Rates
+const kasToSwodnRate = 10; // 1 KAS = 10 SNOWDN (BUY)
+const swodnToKasRate = 0.02; // 1 SNOWDN = 0.02 KAS (SELL)
+const ksdogToSwodnBuyRate = 100000; // 100,000 KSDOG = 1 SNOWDN (BUY)
+const ksdogToSwodnSellRate = 0.001; // 200,000 KSDOG = 1 SNOWDN (SELL)
+const kangoToSwodnRate = 250; // 1 SNOWDN = 250 KANGO (BUY)
+const swodnToKangoRate = 300; // 300 KANGO = 1 SNOWDN (SELL)
+const nachoToSwodnRate = 100; // 1 SNOWDN = 100 NACHO (BUY)
+const swodnToNachoRate = 150; // 150 NACHO = 1 SNOWDN (SELL)
+const usdcToSwodnRate = 500; 
+
+// DOM Elements
+const sendInput = document.getElementById('send-amount');
+const swodnInput = document.getElementById('swodn-amount');
+const buyBtn = document.getElementById('buy-btn');
+const sellBtn = document.getElementById('sell-btn');
+const toggleBtn = document.getElementById('toggle-btn');
+const sendLabel = document.getElementById('send-label');
+const getLabel = document.getElementById('get-label');
+const selectedCurrency = document.getElementById("selected-currency");
+const currencyList = document.getElementById("currency-list");
+
+// Initial State
+let isKasToSwodn = true; // Default mode: You send KAS, get SNOWDN
+let selectedCurrencyValue = 'KAS'; // Default currency
+const maxSnowdn = 1000; // Maximum SNOWDN per transaction
+const minSnowdn = 10; // Minimum SWODN for any transaction
+const minKsdogBuy = 100000; // Minimum KSDOG for buy transaction
+
+sendInput.addEventListener('input', () => {
+  updateExchangeValues();
+});
+
+swodnInput.addEventListener('input', () => {
+  updateReverseExchangeValues();
+});
+
+
+function updateExchangeValues() {
+  const sendAmount = parseFloat(sendInput.value);
+  let calculatedAmount = 0;
+
+  if (!isNaN(sendAmount)) {
+    if (selectedCurrencyValue === 'KAS') {
+      calculatedAmount = isKasToSwodn
+        ? sendAmount * kasToSwodnRate
+        : sendAmount * swodnToKasRate;
+    } else if (selectedCurrencyValue === 'KSDOG') {
+      if (isKasToSwodn && sendAmount >= minKsdogBuy) {
+        calculatedAmount = Math.floor(sendAmount / ksdogToSwodnBuyRate);
+      } else if (!isKasToSwodn) {
+        calculatedAmount = Math.floor(sendAmount / ksdogToSwodnSellRate);
+      }
+    } else if (selectedCurrencyValue === 'KANGO') {
+      calculatedAmount = isKasToSwodn
+        ? sendAmount / kangoToSwodnRate
+        : sendAmount * swodnToKangoRate;
+    } else if (selectedCurrencyValue === 'NACHO') {
+      calculatedAmount = isKasToSwodn
+        ? sendAmount / nachoToSwodnRate
+        : sendAmount * swodnToNachoRate;
+    } else if (selectedCurrencyValue === 'USDC') {
+      calculatedAmount = sendAmount * usdcToSwodnRate;
+    }
+
+
+  
+    calculatedAmount = Math.floor(calculatedAmount);
+    if (calculatedAmount < minSnowdn) {
+      calculatedAmount = 0;
+    } else if (calculatedAmount > maxSnowdn) {
+      calculatedAmount = maxSnowdn;
+      if (selectedCurrencyValue === 'KAS') {
+        sendInput.value = Math.floor(maxSnowdn / kasToSwodnRate);
+      } else if (selectedCurrencyValue === 'KSDOG') {
+        sendInput.value = Math.floor(maxSnowdn * ksdogToSwodnBuyRate);
+      } else if (selectedCurrencyValue === 'KANGO') {
+        sendInput.value = Math.floor(maxSnowdn * kangoToSwodnRate);
+      } else if (selectedCurrencyValue === 'NACHO') {
+        sendInput.value = Math.floor(maxSnowdn * nachoToSwodnRate);
+      } else if (selectedCurrencyValue === 'USDC') {
+        sendInput.value = Math.floor(maxSnowdn / usdcToSwodnRate);
+      }
+    }
+
+    swodnInput.value = calculatedAmount;
+  } else {
+    swodnInput.value = '';
+  }
+}
+
+function updateReverseExchangeValues() {
+  const swodnAmount = parseFloat(swodnInput.value);
+  let calculatedAmount = 0;
+
+  if (!isNaN(swodnAmount)) {
+    if (selectedCurrencyValue === 'KAS') {
+      calculatedAmount = isKasToSwodn
+        ? Math.floor(swodnAmount / kasToSwodnRate)
+        : Math.floor(swodnAmount / swodnToKasRate);
+    } else if (selectedCurrencyValue === 'KSDOG') {
+      calculatedAmount = isKasToSwodn
+        ? swodnAmount * ksdogToSwodnBuyRate
+        : swodnAmount * ksdogToSwodnSellRate;
+    } else if (selectedCurrencyValue === 'KANGO') {
+      calculatedAmount = isKasToSwodn
+        ? swodnAmount * kangoToSwodnRate
+        : swodnAmount / swodnToKangoRate;
+    } else if (selectedCurrencyValue === 'NACHO') {
+      calculatedAmount = isKasToSwodn
+        ? swodnAmount * nachoToSwodnRate
+        : swodnAmount / swodnToNachoRate;
+    } else if (selectedCurrencyValue === 'USDC') {
+      calculatedAmount = swodnAmount / usdcToSwodnRate;
+    }
+
+
+    // Ensure SWODN is an integer and enforce minimum and maximum limits
+    swodnAmountRounded = Math.floor(swodnAmount);
+    if (swodnAmountRounded < minSnowdn) {
+      swodnInput.value = 0;
+      calculatedAmount = 0;
+    } else if (swodnAmountRounded > maxSnowdn) {
+      swodnInput.value = maxSnowdn;
+      if (selectedCurrencyValue === 'KAS') {
+        calculatedAmount = isKasToSwodn
+          ? Math.floor(maxSnowdn / kasToSwodnRate)
+          : Math.floor(maxSnowdn / swodnToKasRate);
+      } else if (selectedCurrencyValue === 'KSDOG') {
+        calculatedAmount = isKasToSwodn
+          ? Math.floor(maxSnowdn * ksdogToSwodnBuyRate)
+          : Math.floor(maxSnowdn * ksdogToSwodnSellRate);
+      } else if (selectedCurrencyValue === 'KANGO') {
+        calculatedAmount = isKasToSwodn
+          ? Math.floor(maxSnowdn / kangoToSwodnRate)
+          : Math.floor(maxSnowdn * swodnToKangoRate);
+      } else if (selectedCurrencyValue === 'NACHO') {
+        calculatedAmount = isKasToSwodn
+          ? Math.floor(maxSnowdn / nachoToSwodnRate)
+          : Math.floor(maxSnowdn * swodnToNachoRate);
+      } else if (selectedCurrencyValue === 'USDC') {
+        calculatedAmount = Math.floor(maxSnowdn / usdcToSwodnRate);
+      }
+    }
+
+    sendInput.value = calculatedAmount.toFixed(2);
+  } else {
+    sendInput.value = '';
+  }
+}
+
+
+toggleBtn.addEventListener('click', () => {
+  isKasToSwodn = !isKasToSwodn;
+  updateLabels();
+  resetInputs();
+});
+
+
+selectedCurrency.addEventListener("click", () => {
+  currencyList.style.display = currencyList.style.display === "block" ? "none" : "block";
+});
+
+currencyList.addEventListener("click", (e) => {
+  if (e.target.tagName === "LI" || e.target.closest("LI")) {
+    let selectedItem = e.target.closest("LI");
+    selectedCurrencyValue = selectedItem.dataset.value;
+    let currencyIcon = selectedItem.dataset.icon;
+
+    selectedCurrency.innerHTML = `
+      <img src="${currencyIcon}" alt="${selectedCurrencyValue} Logo" class="currency-logo" />
+      <span>${selectedCurrencyValue}</span>
+    `;
+
+    currencyList.style.display = "none";
+    updateLabels();
+    resetInputs();
+  }
+});
+
+document.addEventListener("click", (e) => {
+  if (!selectedCurrency.contains(e.target) && !currencyList.contains(e.target)) {
+    currencyList.style.display = "none";
+  }
+});
+
+
+function updateLabels() {
+  sendLabel.textContent = isKasToSwodn ? `You Send (${selectedCurrencyValue}):` : `You Send (SNOWDN):`;
+  getLabel.textContent = isKasToSwodn ? `You Get (SNOWDN):` : `You Get (${selectedCurrencyValue}):`;
+}
+
+
+function resetInputs() {
+  sendInput.value = '';
+  swodnInput.value = '';
+}
+
+function logTransaction(action, currency, amount) {
+  const timestamp = new Date().toLocaleString();
+  const logEntry = `Timestamp: ${timestamp}, Action: ${action}, Currency: ${currency}, Amount: ${amount}\n`;
+
+  fetch('/log-transaction', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ action, currency, amount, timestamp }),
+  })
+  .then(response => response.text())
+  .then(data => {
+    console.log('Transaction logged:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+}
+
+buyBtn.addEventListener('click', () => {
+  const amount = swodnInput.value;
+  if (amount) {
+    logTransaction('Buy', selectedCurrencyValue, amount);
+  }
+  alert('Redirecting to Treasury wallet page...');
+  window.location.href = './buytreasury.html';
+});
+
+sellBtn.addEventListener('click', () => {
+  const amount = swodnInput.value;
+  if (amount) {
+    logTransaction('Sell', selectedCurrencyValue, amount);
+  }
+  alert('Redirecting to Treasury wallet page...');
+  window.location.href = './selltreasury.html';
+});
+
+updateLabels();
---
+++ server.js
@@ -0,0 +1,43 @@
+import 'dotenv/config'; // Load environment variables
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { createClient } from '@supabase/supabase-js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/log-transaction', async (req, res) => {
+    const { action, currency, amount, timestamp } = req.body;
+
+    const { data, error } = await supabase
+        .from('transactions')
+        .insert([{ action, currency, amount, timestamp }]);
+
+    if (error) {
+        console.error('Failed to insert transaction into Supabase', error);
+        return res.status(500).send('Failed to log transaction');
+    }
+
+    res.send('Transaction logged successfully');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
