888bec1978f6f91a75d59899a8c92cf3f3b5ae6e
Akihiko <akihikowiththenfts@gmail.com>
2025-02-08 08:15:41 +0100

    up

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import 'dotenv/config'; 
+import 'dotenv/config'; // Load environment variables
 import express from "express";
 import cors from "cors";
 import path from "path";
@@ -13,7 +13,8 @@ const port = process.env.PORT || 5000;
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseservicerole = process.env.SUPABASE_SERVICE;
+const supabase = createClient(supabaseUrl, supabaseKey, supabaseservicerole);
 
 app.use(cors());
 app.use(express.json());
