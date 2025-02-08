c641ef3237bb591cdf26784503154b8c479f8f6c
Akihiko <akihikowiththenfts@gmail.com>
2025-02-08 10:11:19 +0100

    update

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ const supabaseKey = process.env.SUPABASE_KEY;
 const supabaseservicerole = process.env.SUPABASE_SERVICE;
 const supabase = createClient(supabaseUrl, supabaseKey, supabaseservicerole);
 
-app.use(cors());
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+    allowedHeaders: ['Content-Type', 'Authorization'] 
+}));
+
 app.use(express.json());
 app.use(express.static(__dirname));
 
